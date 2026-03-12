2b6bcc75f20b477d01794a52576ee967fcfb29ea
Meenakshi nagarajan <vinithmeenakshi@gmail.com>
2026-03-12 21:28:17 +0530

    Add files via upload

---
+++ whileloop/while7.java
@@ -0,0 +1,45 @@
+package whileloop;
+import java.util.Scanner;
+
+public class while7 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int choice;
+        int a, b;
+
+        do {
+            System.out.println("Calculator Menu:");
+            System.out.println("1. Add");
+            System.out.println("2. Subtract");
+            System.out.println("3. Multiply");
+            System.out.println("4. Divide");
+            System.out.println("5. Exit");
+
+            System.out.print("Choice: ");
+            choice = sc.nextInt();
+
+            if (choice >= 1 && choice <= 4) {
+                System.out.print("Enter two numbers: ");
+                a = sc.nextInt();
+                b = sc.nextInt();
+
+                if (choice == 1) {
+                    System.out.println("Result: " + (a + b));
+                } 
+                else if (choice == 2) {
+                    System.out.println("Result: " + (a - b));
+                } 
+                else if (choice == 3) {
+                    System.out.println("Result: " + (a * b));
+                } 
+                else if (choice == 4) {
+                    System.out.println("Result: " + (a / b));
+                }
+            }
+
+        } while (choice != 5);
+
+        System.out.println("Goodbye!");
+    }
+}
