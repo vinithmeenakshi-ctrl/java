39dc8744a6826306f756339275349fa1bdc11870
Meenakshi nagarajan <vinithmeenakshi@gmail.com>
2026-03-13 14:07:20 +0530

    Add files via upload

---
+++ whileloop/while12.java
@@ -0,0 +1,25 @@
+package whileloop;
+import java.util.Scanner;
+
+public class while12 {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        int original = num;
+        int reverse = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            reverse = reverse * 10 + digit;
+            num = num / 10;
+        }
+
+        if (original == reverse)
+            System.out.println(original + " is a palindrome");
+        else
+            System.out.println(original + " is not a palindrome");
+    }
+}
+
