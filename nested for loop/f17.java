a2d55650ad6a795712094cc8ea5a6b339a8625a7
Meenakshi nagarajan <vinithmeenakshi@gmail.com>
2026-04-01 19:54:03 +0530

    Add files via upload

---
+++ nested for loop/f17.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class f17 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int height = sc.nextInt();
+        int width = sc.nextInt();
+
+        System.out.println("Hollow Rectangle Pattern:");
+
+        for (int i = 1; i <= height; i++) {
+            for (int j = 1; j <= width; j++) {
+                if (i == 1 || i == height || j == 1 || j == width) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
